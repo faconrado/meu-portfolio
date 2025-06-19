746dc62130297abcc1fc8ebf92a24f42579d6cf1
faconrado <f.conrado88@hotmail.com>
2025-06-18 23:17:20 -0300

    Configuração do application.properties corretamente.

--- fotoapp/src/main/java/com/portfolio/fotoapp/FotoappApplication.java
+++ fotoapp/src/main/java/com/portfolio/fotoapp/FotoappApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FotoappApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FotoappApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(FotoappApplication.class, args);
+    }
 }
